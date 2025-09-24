b32c81555da311ad3f387d7ec91da9e3395ff691
Colin Charles <colincharles1204@gmail.com>
2025-09-25 00:58:33 +0530

    modify app entry point

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv/config";
+import morgan from "morgan";
 import { connectDB } from "./config/db.js";
 import urlRoutes from "./routes/url.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import { handleError } from "./middlewares/handle-error.js";
 
 const app = express();
+
 app.use(express.json());
 
-await connectDB();
+// Morgan HTTP request logging
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/url", urlRoutes);
@@ -17,6 +20,4 @@ app.use("/api/auth", authRoutes);
 // Error handler
 app.use(handleError);
 
-app.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${PORT}`)
-);
+export default app;
